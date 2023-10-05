179f8e7c3a05a9454674e2cbf437bba8a9da4809
Omar-AboJazar <omar.h.abojazar@gmail.com>
2023-10-05 19:44:23 +0300

    JavaScript-Qustion

--- JavaScript_Qustion.js
+++ JavaScript_Qustion.js
@@ -1,23 +1,12 @@
 function missingNumber(nums) {
     const n = nums.length;
-    // Calculate the expected sum from 0 to n
     const expectedSum = (n * (n + 1)) / 2;
-
-    // Calculate the actual sum of elements in the array
     const actualSum = nums.reduce((sum, num) => sum + num, 0);
-
-    // The missing number is the difference between the expected and actual sums
     return expectedSum - actualSum;
 }
-
-// Example 1
 const nums1 = [3, 0, 1];
-console.log(missingNumber(nums1)); // Output: 2
-
-// Example 2
+console.log(missingNumber(nums1)); 
 const nums2 = [9, 6, 4, 2, 3, 5, 7, 0, 1];
-console.log(missingNumber(nums2)); // Output: 8
-
-// Example 3
+console.log(missingNumber(nums2)); 
 const nums3 = [0, 1];
-console.log(missingNumber(nums3)); // Output: 2
+console.log(missingNumber(nums3)); 
